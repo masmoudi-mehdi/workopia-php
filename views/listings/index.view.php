fddcd8e6ae688efa187a385f06217000d112c4b1
masmoudi-mehdi <massmoudi.mehdi95@gmail.com>
2024-09-22 11:46:07 +0100

    added routes and views

--- views/listings/index.view.php
+++ views/listings/index.view.php
@@ -1,10 +1,6 @@
 <?= loadPartial('head') ?>
 <?= loadPartial('navbar') ?>
-<?= loadPartial('showcase-search') ?>
 <?= loadPartial('top-banner') ?>
-
-
-
 
 <!-- Job Listings -->
 <section>
@@ -172,10 +168,7 @@
                 </div>
             </div>
         </div>
-        <a href="/listings" class="block text-xl text-center">
-            <i class="fa fa-arrow-alt-circle-right"></i>
-            Show All Jobs
-        </a>
+
 </section>
 
 
